37300abfa08b6313b35e176f95fbce1398e78606
welchi <welchizm@gmail.com>
2019-04-28 23:47:28 +0900

    add SlackBot class

--- bot.py
+++ bot.py
@@ -8,5 +8,16 @@ class SlackBotTest(unittest.TestCase):
         bot=SlackBot()
         bot.post_message("Hello")
 
+class SlackBot:
+    def __init__(self):
+        self.slack_token=os.environ["SLACK_API_TOKEN"]
+        self.sc=SlackClient(self.slack_token)
+
+    def post_message(self,message):
+        self.sc.api_call(
+            "chat.postMessage",
+            channel="sandbox",
+            text=message
+        )
 if __name__=="__main__":
     unittest.main()
