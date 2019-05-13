c1991796257cc791526435a4f7ec5baf0281c4bb
welchi <welchizm@gmail.com>
2019-05-13 18:42:30 +0900

    change file read interface

--- file_reader.py
+++ file_reader.py
@@ -4,4 +4,7 @@ import unittest
 
 class FileReadTest(unittest.TestCase):
     def test_read_file(self):
-        reader = FileReader("./data/kf1")
+        reader = FileReader()
+        text = reader.read_file("./data/test.txt")
+        self.assertEqual(text, "hoge\nfuga")
+
